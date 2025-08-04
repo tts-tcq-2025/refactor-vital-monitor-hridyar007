5710e3f36258b365a02dfbfea8aed546654a0351
hridyar007 <hridyar007@gmail.com>
2025-08-04 21:49:16 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,34 +1,85 @@
-#include "./monitor.h"
-#include <assert.h>
+#include "monitor.h"
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
 
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-void alert(const char* message) {
-  cout << message << "\n";
-  for (int i = 0; i < 6; i++) {
-    cout << "\r* " << flush;
-    sleep_for(seconds(1));
-    cout << "\r *" << flush;
-    sleep_for(seconds(1));
-  }
+static bool g_test_mode = false;
+
+
+const float TEMP_LOW = 95.0f;
+const float TEMP_HIGH = 102.0f;
+const float PULSE_LOW = 60.0f;
+const float PULSE_HIGH = 100.0f;
+const float SPO2_LOW = 90.0f;
+
+
+void setTestMode(bool enabled) {
+    g_test_mode = enabled;
 }
+
+
+static void flashAlert(const char* message) {
+    std::cout << message << "\n";
+    if (g_test_mode) {
+        // In test mode, do not sleep so tests run fast.
+        return;
+    }
+    
+    for (int i = 0; i < 6; ++i) {
+        std::cout << "\r* " << std::flush;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "\r *" << std::flush;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    std::cout << "\r  \n"; // clean up line
+}
+
+
+AlarmMask evaluateVitals(float temperature, float pulseRate, float spo2) {
+    AlarmMask mask = ALARM_NONE;
+
+    if (temperature < TEMP_LOW) {
+        addAlarm(mask, ALARM_TEMPERATURE_LOW);
+    } else if (temperature > TEMP_HIGH) {
+        addAlarm(mask, ALARM_TEMPERATURE_HIGH);
+    }
+
+    if (pulseRate < PULSE_LOW) {
+        addAlarm(mask, ALARM_PULSE_LOW);
+    } else if (pulseRate > PULSE_HIGH) {
+        addAlarm(mask, ALARM_PULSE_HIGH);
+    }
+
+    if (spo2 < SPO2_LOW) {
+        addAlarm(mask, ALARM_SPO2_LOW);
+    }
+
+    return mask;
+}
+
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    alert("Temperature is critical!");
-    return 0;
-  }
-  if (pulseRate < 60 || pulseRate > 100) {
-    alert("Pulse Rate is out of range!");
-    return 0;
-  }
-  if (spo2 < 90) {
-    alert("Oxygen Saturation out of range!");
-    return 0;
-  }
-  return 1;
-}
+    AlarmMask mask = evaluateVitals(temperature, pulseRate, spo2);
+    if (mask == ALARM_NONE) {
+        return 1;
+    }
 
+    if (isAlarmSet(mask, ALARM_TEMPERATURE_LOW)) {
+        flashAlert("Temperature is too low!");
+    }
+    if (isAlarmSet(mask, ALARM_TEMPERATURE_HIGH)) {
+        flashAlert("Temperature is critical!");
+    }
+    if (isAlarmSet(mask, ALARM_PULSE_LOW)) {
+        flashAlert("Pulse Rate is too low!");
+    }
+    if (isAlarmSet(mask, ALARM_PULSE_HIGH)) {
+        flashAlert("Pulse Rate is too high!");
+    }
+    if (isAlarmSet(mask, ALARM_SPO2_LOW)) {
+        flashAlert("Oxygen Saturation out of range!");
+    }
+
+    return 0;
+}
