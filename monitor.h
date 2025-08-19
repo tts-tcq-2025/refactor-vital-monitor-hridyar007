ae69efb86964cc7b6799691045e98fa999688449
hridyar007 <hridyar007@gmail.com>
2025-08-19 09:32:20 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 
+// Alarm bit masks
 constexpr uint8_t ALARM_NONE              = 0;
 constexpr uint8_t ALARM_TEMPERATURE_LOW   = 1 << 0; // bit 0
 constexpr uint8_t ALARM_TEMPERATURE_HIGH  = 1 << 1; // bit 1
@@ -12,12 +13,17 @@ constexpr uint8_t ALARM_SPO2_LOW          = 1 << 4; // bit 4
 
 using AlarmMask = uint8_t;
 
+// Evaluate patient vitals and return alarm mask
 AlarmMask evaluateVitals(float temperature, float pulseRate, float spo2);
 
+// Returns 1 if vitals are OK, 0 if alarms are triggered.
+// Prints alert messages when alarms occur.
 int vitalsOk(float temperature, float pulseRate, float spo2);
 
+// Enable/disable test mode (skips delays in flashing alerts).
 void setTestMode(bool enabled);
 
+// Helpers
 static inline bool isAlarmSet(AlarmMask mask, AlarmMask alarmBit) {
     return (mask & alarmBit) != 0;
 }
@@ -26,5 +32,4 @@ static inline void addAlarm(AlarmMask &mask, AlarmMask alarmBit) {
     mask |= alarmBit;
 }
 
-#endif
-
+#endif // MONITOR_H
