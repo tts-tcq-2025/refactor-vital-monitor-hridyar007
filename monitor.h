a1350e1725510b3ef2a6d8276223fd5fabd738eb
hridyar007 <hridyar007@gmail.com>
2025-08-25 23:50:21 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,39 +1,25 @@
 #ifndef MONITOR_H
 #define MONITOR_H
 
-#include <cstdint>
+#include "vitals.h"
+#include "alerts.h"
 
-using AlarmMask = uint8_t;
+// Public API
+using AlarmMask = ::AlarmMask;
 
-// Alarm bit masks
-constexpr AlarmMask ALARM_NONE              = 0;
-constexpr AlarmMask ALARM_TEMPERATURE_LOW   = 1 << 0;
-constexpr AlarmMask ALARM_TEMPERATURE_HIGH  = 1 << 1;
-constexpr AlarmMask ALARM_PULSE_LOW         = 1 << 2;
-constexpr AlarmMask ALARM_PULSE_HIGH        = 1 << 3;
-constexpr AlarmMask ALARM_SPO2_LOW          = 1 << 4;
-constexpr AlarmMask ALARM_RESP_LOW          = 1 << 5;
-constexpr AlarmMask ALARM_RESP_HIGH         = 1 << 6;
-
-// Threshold structure for table-driven approach
-struct VitalThreshold {
-    float low;
-    float high;
-    AlarmMask lowAlarm;
-    AlarmMask highAlarm;
-};
-
-AlarmMask evaluateVitals(float temperature, float pulseRate, float spo2, float respirationRate);
-int vitalsOk(float temperature, float pulseRate, float spo2, float respirationRate);
-
-void setTestMode(bool enabled);
-
-// Utility
-inline bool isAlarmSet(AlarmMask mask, AlarmMask alarmBit) {
-    return (mask & alarmBit) != 0;
+// Wrapper to evaluate vitals (logic + optional alerts)
+inline AlarmMask checkVitals(float temperature, float pulseRate, float spo2, float respirationRate) {
+    return evaluateVitals(temperature, pulseRate, spo2, respirationRate);
 }
-inline void addAlarm(AlarmMask& mask, AlarmMask alarmBit) {
-    mask |= alarmBit;
+
+// Wrapper to check vitals and trigger alerts
+inline int checkVitalsWithAlerts(float temperature, float pulseRate, float spo2, float respirationRate) {
+    return vitalsOk(temperature, pulseRate, spo2, respirationRate);
+}
+
+// Expose test mode control
+inline void enableTestMode(bool enabled) {
+    setTestMode(enabled);
 }
 
 #endif // MONITOR_H
