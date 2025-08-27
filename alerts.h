e8e408fce08977d58a40ed12aa91f6b238aed65d
hridyar007 <hridyar007@gmail.com>
2025-08-27 14:19:45 +0530

    Update alerts.h

--- alerts.h
+++ alerts.h
@@ -3,9 +3,17 @@
 
 #include "vitals.h"
 #include <string>
+#include <vector>
 
+// Side-effect functions
 void flashAlert(const std::string& message);
 void setTestMode(bool enabled);
+
+// Logic separated from I/O
+std::vector<std::string> getAlarmMessages(AlarmMask mask);
+void showAlarms(AlarmMask mask);
+
+// Combined check + alert
 int vitalsOk(float temperature, float pulseRate, float spo2, float respirationRate);
 
 #endif // ALERTS_H
